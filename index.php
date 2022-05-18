8c920c52efa9b9a4623f685e93a3633f12209bda
SleepyMeepo <96211945+SleepyMeepo@users.noreply.github.com>
2022-05-18 04:07:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>COMP 3015 Lab 2</title>
 </head>
 <body>
-    <p>Lab 2 by Raymond Yan</p>
+    <p>Lab 2 by Raymond Yan A01208607</p>
     <?php
 
         function getDayOfTheWeek($year,$month,$day){
